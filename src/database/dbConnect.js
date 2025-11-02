c275f8e9240dfd4313ccd98d88a7c213156b8d4a
Rahmat <ash.vyracorporation@gmail.com>
2025-11-02 11:16:49 +0700

    Create dbConnect.js

---
+++ src/database/dbConnect.js
@@ -0,0 +1,22 @@
+// src/database/dbConnect.js
+import { google } from "googleapis";
+
+export const connectGoogle = async () => {
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
+      type: "service_account",
+      project_id: process.env.GOOGLE_PROJECT_ID,
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      client_email: process.env.GOOGLE_SERVICE_EMAIL,
+    },
+    scopes: [
+      "https://www.googleapis.com/auth/spreadsheets",
+      "https://www.googleapis.com/auth/drive",
+    ],
+  });
+
+  const sheets = google.sheets({ version: "v4", auth });
+  const drive = google.drive({ version: "v3", auth });
+
+  return { sheets, drive };
+};
